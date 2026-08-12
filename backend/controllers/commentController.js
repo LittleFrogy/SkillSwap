85ca161dd2eb8c605bfa386c0f5450c0fcb20625
Tamim Hasan Akib <tamimhasanakib@gmail.com>
2026-08-12 21:36:58 +0600

    fix(module2): fix case-sensitive model imports in community controllers for render deployment

--- backend/controllers/commentController.js
+++ backend/controllers/commentController.js
@@ -1,5 +1,5 @@
-const Comment = require("../models/comment");
-const Post = require("../models/post");
+const Comment = require("../models/Comment");
+const Post = require("../models/Post");
 
 // Get comments for a post
 exports.getComments = async (req, res) => {
--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -1,4 +1,4 @@
-const Post = require("../models/post");
+const Post = require("../models/Post");
 
 // Get all posts
 exports.getPosts = async (req, res) => {
